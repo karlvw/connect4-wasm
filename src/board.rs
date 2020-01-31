6495743a54cceb6be739e6140f1adfab957589a4
Karl van Workum <karl.vanworkum@gmail.com>
2020-02-01 10:34:12 +1300

    Speeding up board checking

--- src/board.rs
+++ src/board.rs
@@ -73,9 +73,7 @@ impl Board {
     /// Check to see if the game has a winner or a draw
     pub fn check_winner(&self) -> Option<GameResult> {
         let check_group = |c1, c2, c3, c4| -> Option<GameResult> {
-            if c1 == Cell::Empty {
-                None
-            } else if (c1 == c2) && (c2 == c3) && (c3 == c4) {
+            if (c1 == c2) && (c2 == c3) && (c3 == c4) {
                 match c1 {
                     Cell::Player => Some(GameResult::PlayerWins),
                     Cell::Computer => Some(GameResult::ComputerWins),
@@ -86,34 +84,38 @@ impl Board {
             }
         };
 
-        // Check for horizontal sequences
         for row in 0..NUM_ROWS {
-            for col in 0..(NUM_COLUMNS-3) {
-                if let Some(result) = check_group(self.cells[row][col], self.cells[row][col+1], self.cells[row][col+2], self.cells[row][col+3]) {
-                    return Some(result);
-                }
-            }
-        }
+            for col in 0..NUM_COLUMNS {
+                let c1 = self.cells[row][col];
 
-        // Check for vertical sequences
-        for col in 0..NUM_COLUMNS {
-            for row in 0..(NUM_ROWS-3) {
-                if let Some(result) = check_group(self.cells[row][col], self.cells[row+1][col], self.cells[row+2][col], self.cells[row+3][col]) {
-                    return Some(result);
+                if c1 == Cell::Empty {
+                    continue;
                 }
-            }
-        }
 
-        // Check for diagonal sequences
-        for row in 0..(NUM_ROWS-3) {
-            for col in 0..(NUM_COLUMNS-3) {
-                if let Some(result) = check_group(self.cells[row][col], self.cells[row+1][col+1], self.cells[row+2][col+2], self.cells[row+3][col+3]) {
-                    return Some(result);
+                // Check for horizontal sequences
+                if col < (NUM_COLUMNS-3) {
+                    if let Some(result) = check_group(c1, self.cells[row][col+1], self.cells[row][col+2], self.cells[row][col+3]) {
+                        return Some(result);
+                    }
                 }
-            }
-            for col in 3..(NUM_COLUMNS) {
-                if let Some(result) = check_group(self.cells[row][col], self.cells[row+1][col-1], self.cells[row+2][col-2], self.cells[row+3][col-3]) {
-                    return Some(result);
+
+                if row < (NUM_ROWS-3) {
+                    // Check for vertical sequences
+                    if let Some(result) = check_group(c1, self.cells[row+1][col], self.cells[row+2][col], self.cells[row+3][col]) {
+                        return Some(result);
+                    }
+
+                    // Check for diagonal sequences
+                    if col < (NUM_COLUMNS-3) {
+                        if let Some(result) = check_group(c1, self.cells[row+1][col+1], self.cells[row+2][col+2], self.cells[row+3][col+3]) {
+                            return Some(result);
+                        }
+                    }
+                    if col >= 3 {
+                        if let Some(result) = check_group(c1, self.cells[row+1][col-1], self.cells[row+2][col-2], self.cells[row+3][col-3]) {
+                            return Some(result);
+                        }
+                    }
                 }
             }
         }
